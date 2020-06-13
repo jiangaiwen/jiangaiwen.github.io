30e91a9f0d1bd3ce81d0e875e67933677b918735
jawen <jawen.9@outlook.com>
2020-06-13 10:39:43 +0800

    delete

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # jawen.github.io
 
-# 主题颜色设置
+# 主题设置
 1、添加sass-resources-loader
 2、build > utils.js 添加mixin
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-    device: state => state.app.device,
-    theme: state => state.app.theme
-}
-
-export default getters
